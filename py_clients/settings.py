190728fcf3704e6c0b08fa6546bd817dd6a42796
Lyebert <fluvio.sistema@gmail.com>
2021-12-14 20:22:29 -0300

    Conexao com BD

--- py_clients/settings.py
+++ py_clients/settings.py
@@ -75,8 +75,13 @@ WSGI_APPLICATION = 'py_clients.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'fundamentos',
+        'PORT': '3306',
+        'USER': 'root',
+        'PASSWORD': 'Argonath21@',
+        'HOST': '127.0.0.1'
+
     }
 }
 
