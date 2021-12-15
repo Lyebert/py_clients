39758116d0f71b46db050db1f0f487f5a2700580
Lyebert <fluvio.sistema@gmail.com>
2021-12-15 20:15:02 -0300

    migrategit status

--- clients/models.py
+++ clients/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 
 
-
 class Cliente(models.Model):
     SEXO_CHOICES = (
         ("F", "FEMININO"),
@@ -9,26 +8,10 @@ class Cliente(models.Model):
         ("N", "NENHUMA DAS OPÇÕES")
     )
     nome = models.CharField(max_length=100, null=False, blank=False)
-    data_nascimento = models.DateField(null=False, blanck=False)
+    data_nascimento = models.DateField(null=False, blank=False)
     email = models.EmailField(null=False, blank=False)
     profissao = models.CharField(max_length=50, null=False, blank=False)
     sexo = models.CharField(max_length=1, choices=SEXO_CHOICES, blank=False, null=False)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    def __str__(self):
+        return self.nome
